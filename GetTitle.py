68ce17d7fe6c1ef7820366acdd3a90e9ea78a0d9
temp176 <teheodks@gmail.com>
2018-06-10 19:52:07 +0900

    first commit

---
+++ GetTitle.py
@@ -0,0 +1,49 @@
+from urllib3.exceptions import InsecureRequestWarning
+from datetime import datetime
+from time import sleep
+import urllib3
+import json
+import config
+
+urllib3.disable_warnings(InsecureRequestWarning)
+
+API_URL = config.API_URL
+FILE_NAME = config.FILE_NAME
+LOG_NAME = config.LOG_NAME
+SLEEP_TIME = config.SLEEP_TIME
+START_ID = config.START_ID
+END_ID = config.END_ID
+
+def titleDataToList(dicData):
+    titles = []
+    for i in range(1,len(dicData)):
+        titles.append(list(dicData[i].values())[0])
+    return titles
+
+def getTitleData(userid):
+    http = urllib3.PoolManager()
+    r = http.request('GET',API_URL,fields={'out':'json','of':'t','userid':userid})
+    dicData = json.loads(r.data)
+    return titleDataToList(dicData)
+
+def writeTitleData(titles):
+    fout = open(FILE_NAME,'a')
+    print(*titles, sep='\n',file = fout)
+    fout.close()
+
+def writeLog(logText):
+    fout = open(LOG_NAME,'a')
+    print(logText, sep='\n',file = fout)
+    fout.close()
+
+
+for userID in range(START_ID,END_ID + 1):
+    titles = getTitleData(userID)
+    titles = list(filter(lambda str:str != '', titles))
+    if len(titles) != 0:
+        writeTitleData(titles)
+    nowTime = datetime.now()
+    logText = 'Time:[{}] ID:{} Count:{}'.format(nowTime,userID,len(titles))
+    print(logText)
+    writeLog(logText)
+    sleep(SLEEP_TIME)
---
+++ config.py
@@ -0,0 +1,17 @@
+#APIのURL
+API_URL = 'https://api.syosetu.com/novelapi/api'
+
+#タイトルを書き出すファイルの名前
+FILE_NAME = 'title.txt'
+
+#ログを書き出すファイルの名前
+LOG_NAME = 'log.txt'
+
+#リクエスト間隔(アクセス負荷対策)
+SLEEP_TIME = 1.0
+
+#取得を開始するユーザID
+START_ID = 1
+
+#取得を終了するユーザID
+END_ID = 10
