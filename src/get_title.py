78fbc444632bc2e78c368d92dd5fa2d68434326f
temp176 <teheodks@gmail.com>
2018-07-20 15:18:26 +0900

    Change file name and function names

--- src/get_title.py
+++ src/get_title.py
@@ -15,36 +15,36 @@ SLEEP_TIME = config.SLEEP_TIME
 START_ID = config.START_ID
 END_ID = config.END_ID
 
-def titleDataToList(dicData):
+def title2list(dicData):
     titles = []
     for i in range(1,len(dicData)):
         titles.append(list(dicData[i].values())[0])
     return titles
 
-def getTitleData(userid):
+def get_title(userid):
     http = urllib3.PoolManager()
     r = http.request('GET',API_URL,fields={'out':'json','of':'t','userid':userid})
     dicData = json.loads(r.data)
-    return titleDataToList(dicData)
+    return title2list(dicData)
 
-def writeTitleData(titles):
+def write_title(titles):
     fout = open(FILE_NAME,'a')
     print(*titles, sep='\n',file = fout)
     fout.close()
 
-def writeLog(logText):
+def write_log(logText):
     fout = open(LOG_NAME,'a')
     print(logText, sep='\n',file = fout)
     fout.close()
 
 
 for userID in range(START_ID,END_ID + 1):
-    titles = getTitleData(userID)
+    titles = get_title(userID)
     titles = list(filter(lambda str:str != '', titles))
     if len(titles) != 0:
-        writeTitleData(titles)
+        write_title(titles)
     nowTime = datetime.now()
     logText = 'Time:[{}] ID:{} Count:{}'.format(nowTime,userID,len(titles))
     print(logText)
-    writeLog(logText)
+    write_log(logText)
     sleep(SLEEP_TIME)
